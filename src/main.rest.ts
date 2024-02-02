9a23422b028b751e9ef0e5e4defc5ab9bb9e73c1
maksim.samorukov <maksim.samorukov@sphaera.ru>
2024-02-02 19:10:36 +0300

    added Label Component

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -5,12 +5,14 @@ import { Component } from './shared/types/index.js';
 import { RestApplication } from './rest/index.js';
 import { Logger, ILogger } from './shared/libs/logger/index.js';
 import { Config, IConfig, RestSchema } from './shared/libs/config/index.js';
+import { ILabel, Label } from './shared/libs/label/index.js';
 
 async function bootstrap() {
   const container = new Container();
   container.bind<RestApplication>(Component.RestApplication).to(RestApplication).inSingletonScope();
   container.bind<ILogger>(Component.Logger).to(Logger).inSingletonScope();
   container.bind<IConfig<RestSchema>>(Component.Config).to(Config).inSingletonScope();
+  container.bind<ILabel>(Component.Label).to(Label).inSingletonScope();
 
   const restApplication = container.get<RestApplication>(Component.RestApplication);
   await restApplication.init();
---
+++ src/shared/libs/label/labels.ts
@@ -0,0 +1,9 @@
+export const labels = {
+  common: {
+    noTranslation: 'Нет перевода',
+  },
+  config: {
+    errorReadingEnvFile: 'Error reading .env file',
+    envFileSuccessfullyParsed: '.env file successfully parsed',
+  }
+}
